393696fc3def914d23f49ae27a227cad7014ed50
Connor Love <connor@dotconnor.com>
2018-10-14 23:12:02 -0400

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,9 @@
-declare module "@a1motion/driveweb" {
-  class DriveInterface {
-    constructor(ip:string);
-    getParameter(id: number): Promise<number>;
-    getParameters(ids: number[]): Promise<number[]>;
-    setParameter(id: number, value: number): Promise<number>;
-    setParameters(IdsAndValues: {
-      [id:number]: number
-    }): Promise<number[]>;
-  }
-  export = DriveInterface;
+export = class DriveInterface {
+  constructor(ip:string);
+  getParameter(id: number): Promise<number>;
+  getParameters(ids: number[]): Promise<number[]>;
+  setParameter(id: number, value: number): Promise<number>;
+  setParameters(IdsAndValues: {
+    [id:number]: number
+  }): Promise<number[]>;
 }
